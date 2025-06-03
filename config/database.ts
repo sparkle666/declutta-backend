f4d103b448e325a9de3b7d26a4bd1191ec3ea61c
Sparkle666 <sikky606@gmail.com>
2025-06-03 08:29:02 +0100

    FEAT: added postgress with neondb switched from sqlite

--- config/database.ts
+++ config/database.ts
@@ -1,8 +1,8 @@
-// import app from '@adonisjs/core/services/app'
+import env from '#start/env'
 import { defineConfig } from '@adonisjs/lucid'
 
 const dbConfig = defineConfig({
-  connection: 'sqlite',
+  connection: 'pg', // Change to 'pg' if you want PostgreSQL as default
   connections: {
     sqlite: {
       client: 'better-sqlite3',
@@ -10,6 +10,23 @@ const dbConfig = defineConfig({
         filename: './database/db.sqlite3' // Changed path
       },
       useNullAsDefault: true,
+      migrations: {
+        naturalSort: true,
+        paths: ['database/migrations'],
+      },
+    },
+    pg: {
+      client: 'pg',
+      connection: {
+        host: env.get('PG_HOST'),
+        port: env.get('PG_PORT') ? Number(env.get('PG_PORT')) : undefined,
+        user: env.get('PG_USER'),
+        password: env.get('PG_PASSWORD'),
+        database: env.get('PG_DB_NAME'),
+        ssl: env.get('PG_SSL', 'false') === 'true'
+          ? { rejectUnauthorized: false }
+          : false,
+      },
       migrations: {
         naturalSort: true,
         paths: ['database/migrations'],
